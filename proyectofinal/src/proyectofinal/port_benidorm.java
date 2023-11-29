2107649b986a9a6b92d3f12b167c3c4e9cd5b407
basilio007 <113118989+basilio007@users.noreply.github.com>
2023-11-29 17:11:26 +0100

    newpictures

--- proyectofinal/src/proyectofinal/port_benidorm.java
+++ proyectofinal/src/proyectofinal/port_benidorm.java
@@ -9,6 +9,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.sql.Connection;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -29,26 +30,40 @@ public class port_benidorm extends JFrame{
 		    setSize(1080, 720);
 	        setLocationRelativeTo(null);
 	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	       
 
 	        // Color de fondo
 	        Color colorDeFondo = new Color(15, 82, 15, 255);
 	       getContentPane().setBackground(colorDeFondo);
-
 	       JPanel panelInferior = new JPanel(new BorderLayout());
-	        panelInferior.setBackground(new Color(255, 255, 255, 255));
+	       panelInferior.setBackground(new Color(255, 255, 255, 255));
+	       this.getContentPane().add(panelInferior);
+
+	       JLabel anuncio1 = new JLabel();
+	       ImageIcon foto = new ImageIcon("imagenes/PortBenHotl2.jpg");
+	       anuncio1.setIcon(new ImageIcon(foto.getImage().getScaledInstance(190, 170, Image.SCALE_SMOOTH)));
+
+	       // Establecer un margen para el JLabel
+	       anuncio1.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 0));
+
+	       panelInferior.add(anuncio1, BorderLayout.WEST);
+	       anuncio1.setVisible(true);
+
 	        
-	       /* JButton VerAnuncio2=new JButton("Ver Anuncio");
+	        
+	       JButton VerAnuncio2=new JButton("Reservar");
 	        VerAnuncio2.setBounds(720, 480, 150, 50);
 	        VerAnuncio2.setBackground(new Color(213,232,212,255));
 	        //ImageIcon botonimagen = new ImageIcon("Pictures/back5.png");
 	        //atras.setIcon(new ImageIcon(botonimagen.getImage().getScaledInstance(atras.getWidth(),atras.getHeight(),Image.SCALE_SMOOTH )));
-	        panelInferior.add(VerAnuncio2);
-	        VerAnuncio2.setVisible(true);*/
+	        panelInferior.add(VerAnuncio2, BorderLayout.SOUTH);
+	        VerAnuncio2.setVisible(true);
 	       
 	     // Panel superior (que cumple la funci�n de una barra de men�)
 	        JPanel barraMenu = new JPanel(new BorderLayout());
 	        barraMenu.setBackground(new Color(213, 232, 212, 255)); // Color verde lim�n
 	        barraMenu.setPreferredSize(new Dimension(1050, 80));
+	        this.getContentPane().add(barraMenu);
 
 	        // Icono
 	        ImageIcon icono = new ImageIcon("imagenes/casa.png");
